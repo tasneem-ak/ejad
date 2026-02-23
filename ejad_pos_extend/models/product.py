110e9d351f8b66a29f563917054f2e517b823c61
plennix <tasneem.alsammani@plennix.com>
2026-02-23 22:09:50 +0200

    POS Task 2

--- ejad_pos_extend/models/product.py
+++ ejad_pos_extend/models/product.py
@@ -1,4 +1,4 @@
-from odoo import api, models
+from odoo import _, api, models
 
 
 class ProductProduct(models.Model):
@@ -9,4 +9,31 @@ class ProductProduct(models.Model):
         picking_type = self.env['stock.picking.type'].browse(picking_type_id)
         warehouse = picking_type.warehouse_id
         products = self.with_context(warehouse=warehouse.id).browse(product_ids)
-        return {str(p.id): p.qty_available for p in products}
+        stock_data = {str(p.id): p.qty_available for p in products}
+
+        low_stock_products = products.filtered(lambda p: p.qty_available <= 5)
+        if low_stock_products:
+            self._notify_warehouse_admins(low_stock_products, warehouse)
+
+        return stock_data
+
+    def _notify_warehouse_admins(self, products, warehouse):
+        admin_group = self.env.ref('stock.group_stock_manager')
+        admin_partners = admin_group.users.mapped('partner_id')
+        if not admin_partners:
+            return
+        for product in products:
+            product.product_tmpl_id.message_post(
+                body=_(
+                    "POS Alert: Product <b>%(product)s</b> has low stock "
+                    "(%(qty)s units) in warehouse <b>%(warehouse)s</b>. "
+                    "Please check the Re-Order Point.",
+                    product=product.display_name,
+                    qty=product.qty_available,
+                    warehouse=warehouse.name,
+                ),
+                subject=_("Low Stock Alert from POS"),
+                partner_ids=admin_partners.ids,
+                message_type='notification',
+                subtype_xmlid='mail.mt_note',
+            )
